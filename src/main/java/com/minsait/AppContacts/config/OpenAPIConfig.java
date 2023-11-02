c73567ded4e3548b152a55e5d370b55c44c12872
itaji-create <itaji.1999@gmail.com>
2023-11-02 19:37:58 -0300

    adiciona informações de contato e segurança na classe OpenAPI e cria bens para a politica de cors

--- src/main/java/com/minsait/AppContacts/config/OpenAPIConfig.java
+++ src/main/java/com/minsait/AppContacts/config/OpenAPIConfig.java
@@ -3,17 +3,28 @@ package com.minsait.AppContacts.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class OpenAPIConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-            .info(new Info()
-                .title("Contacts Application")
-                .version("1.0")
-                .description("API made to consolidate knowledge obtained in Minsait's Java Training course."));
+        		.components(new Components().addSecuritySchemes("basicScheme",
+						new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+	            .info(new Info()
+	                .title("Contacts Application")
+	                .version("1.0")
+	                .description("API made to consolidate knowledge obtained in Minsait's Java Training course.")
+	                .contact(new Contact()
+		                    .name("Itaji de Carvalho")
+		                    .url("https://github.com/itaji-create")
+		                    .email("ctt.itaji.carvalho@gmail.com")
+		             )
+	             );
     }
 }
--- src/main/java/com/minsait/AppContacts/config/SecurityConfiguration.java
+++ src/main/java/com/minsait/AppContacts/config/SecurityConfiguration.java
@@ -4,20 +4,47 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@Configuration
+
 @EnableWebSecurity
+@Configuration
 public class SecurityConfiguration {
 
-    @Bean
+	@Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
+	
+	@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("https://appcontacts-production.up.railway.app");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+	
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+	    return (web) -> {
+	        web.ignoring().requestMatchers("/**.html", "/v3/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**", "/swagger-ui.html");
+	    };
+	}
 
 }
